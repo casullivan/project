40f882a675f6bdc19a8e128147015e28e3f7e32a
Caleb <csullivan@bravecs.com>
2014-12-05 09:53:01 -0600

    testfix

--- r_r.py
+++ r_r.py
@@ -16,16 +16,18 @@ def connect():
     try:
         remote.set("registry", local.get("registry"))
     except Exception as e:
+        print e, "failed connect"
+        connect()
         pass
     finally:
-        time.sleep(poll_rate)
+        pass
 
 def run():
     time.sleep(poll_rate)
     try:
         for item in tags:
             remote.set(item, local.get(item))
-
+            
         remote.set('opc_server', local.get('opc_server'))
         remote.set('connected', local.get('connected'))
         remote.set('opc_server', local.get('opc_server'))
@@ -33,6 +35,7 @@ def run():
         remote.set('opc_server_name', local.get('opc_server_name'))
     
     except Exception as e:
+        print e, "failed run"
         connect()
         pass
     finally:
@@ -44,7 +47,7 @@ local_redis_server = 'localhost'
 remote_redis_password = "hjrDouFuQPhNuhmL"
 remote_redis_server = "pub-redis-19711.us-east-1-4.4.ec2.garantiadata.com"
 remote_redis_port = 19711
-poll_rate = 0.3
+poll_rate = 2.0
 
 local = Redis(host=local_redis_server)
 remote = Redis(host=remote_redis_server, port=remote_redis_port, password=remote_redis_password)
@@ -54,6 +57,8 @@ log = logging.getLogger(__name__)
 
 #attempt to connect
 connect()
+
+tags = json.loads(local.get("registry"))
 
 # start service
 while True:
